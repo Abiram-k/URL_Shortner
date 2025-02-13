348357ab69a1c4017a88efde1a427afec08cc98c
Abiram-k <abiramk0107@gmail.com>
2025-02-13 17:48:53 +0530

    production:vercel.json

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
-app.use("/", urlRoutes);
+app.use("/api", urlRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 

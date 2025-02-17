39758dc83d96eb47f2103e2ffe4ab1afd67e60e8
Abiram-k <abiramk0107@gmail.com>
2025-02-17 18:50:42 +0530

    added vercel.json

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const urlRoutes = require("./routes/urlRoutes");
+const urlRoutes = require("../routes/urlRoutes");
 
 const app = express();
 app.use(cors());
@@ -10,7 +10,7 @@ app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
+  .catch((err) => console.log(err)); 
 
 app.use("/api", urlRoutes);
 

c900ba6e311fb981b5ad0cb87dbfbfd4ea5e5395
Abiram-k <abiramk0107@gmail.com>
2025-02-17 20:03:06 +0530

    fixing cors

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -5,12 +5,16 @@ const cors = require("cors");
 const urlRoutes = require("../routes/urlRoutes");
 
 const app = express();
-app.use(cors());
-app.use(express.json());
+
+app.use(cors({
+  origin: 'https://smolink-iota.vercel.app',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+})); app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err)); 
+  .catch((err) => console.log(err));
 
 app.use("/api", urlRoutes);
 
